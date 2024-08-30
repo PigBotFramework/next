e5579a85725637e03e5a80f6ad5ca608f440fb52
gingmzmzx <2417481092@qq.com>
2024-08-30 20:53:14 +0800

    docs: 完善docstring

--- pbf/controller/Client.py
+++ pbf/controller/Client.py
@@ -1,3 +1,91 @@
+"""
+# Client包的使用方法
+
+## Msg类的使用
+
+### 构造器定义
+```python
+Msg(*messages, event: pbf.controller.Data.Event = None)
+```
+您可以传入多个消息，消息可以是str或Statement对象。str中可以包含CQ码，会自动转为Statement对象。<br>
+```python
+msg1 = [
+    TextStatement("这是一段消息"),
+    FaceStatement(114)
+]
+
+msg2 = [
+    FaceStatement(514),
+    "str类型会自动被转为TextStatement",
+    TextStatement("这是另一段文本")
+]
+
+msg3 = [
+    "[CQ:face,id=191]",  # 若str中含有CQ码，会自动被转换！
+    TextStatement("这是一段text消息"),
+    "这是另一段text消息"
+]
+```
+上面三种传参方式都是可以的
+
+`event`参数是当前正在处理的事件，您可以选择不传入。<br>
+如果传入`event`，后续调用`send`方法的时候将会自动选择事件中对应的用户或群聊发送<br>
+如果不传入，您需要调用`send_to`方法指定用户或群聊ID并发送
+
+### `send`方法
+```python
+send(self, retry: int = 0, image: bool = True)
+```
+- `retry`表示重试次数，`0`表示不重试
+- `image`表示是否尝试自动转为图片发送
+
+当`retry`次数耗尽时，若`image`传参为`True`，就会尝试自动将消息转为图片发送。
+
+### `send_to`方法
+```python
+send_to(self, user_id: int = None, group_id: int = None)
+```
+顾名思义，您需要传入`user_id`和`group_id`中的至少一项。<br>
+如果两者都传入，那么会优先向`user_id`用户发送消息<br>
+如果两者都为None，那么会抛出`ValueError("No user_id or group_id specified!")`
+
+
+##  Client类的使用
+Client类用来与OneBot实现直接进行交互，调用接口。Msg类也是继承Client类实现的。
+
+### 构造器
+```python
+Client(event: pbf.controller.Data.Event = None)
+```
+`event`参数可以传入，如果不传入，就不能使用`request_by_event`方法
+
+### `request`方法
+```python
+request(self, action: str, data=None, echo: str = "")
+```
+**关于每一个参数的详细解释，见[OneBot v12文档](https://12.onebot.dev/connect/data-protocol/action-request/)** <br>
+- `action`表示动作名称，如`send_message`
+- `data`是动作参数，即OneBot文档中的`params`
+- `echo`是回声，可以用于唯一标识一个动作请求
+
+该方法会根据`pbf.config.ob_version`的值自动适配不同的OneBot标准<br>
+`pbf.config.ob_version`的可选值有`"v11"`和`"v12"`<br>
+返回请求下来的json解码后的数据
+
+### `request_by_event`方法
+使用该方法，可以很方便地根据`event`中的数据来自动填充动作参数，比如：
+```python
+# 下面的例子均采用OneBot v12标准
+
+Client(event).request_by_event("delete_message", ["message_id"])  # 撤回当前事件对应的消息
+# 这样可以自动从`event`中提取出`message_id`并发送请求
+
+Client(event).request_by_event("send_message", ["detail_type", "group_id"], {"message":"Hello"})  # 发送消息到当前事件对应的群里
+# 这样可以在`detail_type`和`group_id`的基础上，再添加`message`参数发送请求
+```
+"""
+
+
 from ..setup import logger, pluginsManager
 from .. import config
 from .. import version
--- pbf/statement/__init__.py
+++ pbf/statement/__init__.py
@@ -1,3 +1,63 @@
+"""
+# Statement使用方法
+您可以通过pbf.statement.Statement快速构建新的Statement，仅需要通过继承pbf.statement.Statement类
+
+下面是一个使用例子
+```python
+from pbf.statement import Statement
+
+class VideoStatement(Statement):
+    def __init__(self, id: int, video_url: str) -> None:
+        super().__init__("video", **{"video_url": video_url, "id": id})
+
+# 或者像下面这样定义（更直观，但是IDE可能会要求你调用超类的__init__方法）
+
+class VideoStatement(Statement):
+    cqtype: str = "video"
+    id: int = None
+    video_url: str = None
+
+    def __init__(self, id: int, video_url: str) -> None:
+        self.id = id
+        self.video_url = video_url
+```
+然后我们可以在Msg中传入该Statement进行发送
+```python
+from pbf.controller.Client import Msg
+from pbf.statement.TextStatement import TextStatement
+from pbf.statement.FaceStatement import FaceStatement
+from path.to.your.statement import VideoStatement
+
+msg = [
+    TextStatement("这是一段文字"),
+    FaceStatement(114),  # 这是一个表情，id为151
+    VideoStatement(514, "https://example.com")
+]
+Msg(*msg).send_to(group_id=114514)
+```
+这样做会发送这样的一段消息：
+```text
+这是一段文字[CQ:face,id=114][CQ:video,id=514,video_url=https://example.com]
+```
+
+
+# 为FaceStatement适配Lagrange
+如果你在使用Lagrange.OneBot，那你可能需要对FaceStatement进行一些修改来适配Lagrange
+
+FaceStatement的构造是这样的`[CQ:face,id=xxx]`，其中`xxx`为`int`类型，指明表情的ID。<br>
+这样做在go-cqhttp中没有任何问题，但是在Lagrange中会引起JSON解析错误，您需要覆写将`id`类型替换为`str` <br>
+```python
+from pbf.statement import Statement
+
+class FaceStatement(Statement):
+    id: str = None
+    cqtype = "face"
+
+    def __init__(self, id: int):
+        self.id = str(id)
+```
+"""
+
 class Statement:
     cqtype: str = None
     statementFlag: bool = False
