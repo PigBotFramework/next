1e2d34f3e6c1dec18e5ca7acffa949f2c13f2cf9
gingmzmzx <2417481092@qq.com>
2024-10-27 09:25:25 +0800

    docs: add more docs

---
+++ pbf/docs/__init__.py
@@ -0,0 +1,19 @@
+"""
+# Documentation
+您可以在这里学习PBF插件编写的进阶技巧
+
+## Contents
+- CLI命令行
+- 事件处理机制
+- 插件管理器 *
+- 监听器管理 *
+- 缓存模块 (a.k.a. Placeholder)
+- MSC开发模式
+- Statement的进阶使用
+- Model的使用 (Peewee)
+- 工具模块的进阶使用
+- 监听器注册 (`pbf.utils.Register`)
+- Driver驱动 *
+
+### To be continued...
+"""
